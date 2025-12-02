bb80d9bb5fe121dac13e272610d7d65d6813b9c0
Dale Bingham <dale.bingham@soteriasoft.com>
2025-12-02 10:43:07 -0500

    fix the o.Authority = jwtAuthorityServer line

--- src/Startup.cs
+++ src/Startup.cs
@@ -77,7 +77,7 @@ namespace openrmf_api_controls
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             }).AddJwtBearer(o =>
             {
-                o.Authority = jwtAuthorityServer + "realms/openrmf";
+                o.Authority = jwtAuthorityServer;
                 o.Audience = Environment.GetEnvironmentVariable("JWTCLIENT");
                 o.IncludeErrorDetails = true;
                 o.RequireHttpsMetadata = false;
