2be25030a20db817e544c9c48f72198dfaf6b53e
Dale Bingham <dale.bingham@soteriasoft.com>
2025-12-02 10:35:57 -0500

    fixing the JWT Auth code

--- src/Startup.cs
+++ src/Startup.cs
@@ -55,9 +55,9 @@ namespace openrmf_api_controls
 
             foreach(string jwtServer in Environment.GetEnvironmentVariable("JWTAUTHORITY").Split(',')) {
                 if (jwtServer.EndsWith('/'))
-                    jwtAuthorities.Add((jwtServer + "realms/openrmf").Trim().ToLower());
+                    jwtAuthorities.Add((jwtServer).Trim().ToLower());
                 else 
-                    jwtAuthorities.Add((jwtServer + "/realms/openrmf").Trim().ToLower());
+                    jwtAuthorities.Add((jwtServer + "/").Trim().ToLower());
             }
 
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JWTINTERNALAUTHORITY"))) {
